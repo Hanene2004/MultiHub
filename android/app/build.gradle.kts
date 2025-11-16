da91c3300e073a4ebe9e50e978a3ffca68f092c8
Hanene2004 <hanane.ghabbara@gmail.com>
2025-11-16 20:09:36 +0100

    Fix build.gradle.kts by removing merge conflict markers
    - Removed all merge conflict markers (<<<<<<<, =======, >>>>>>>)
    - Set namespace and applicationId to 'com.example.my_app_new'
    - Cleaned up the Kotlin DSL structure

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,30 +6,22 @@ plugins {
 }
 
 android {
-<<<<<<< HEAD
     namespace = "com.example.my_app_new"
-=======
-    namespace = "com.example.my_app"
->>>>>>> a91dba3157045feb26350301988471fefc93d873
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-<<<<<<< HEAD
         applicationId = "com.example.my_app_new"
-=======
-        applicationId = "com.example.my_app"
->>>>>>> a91dba3157045feb26350301988471fefc93d873
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
